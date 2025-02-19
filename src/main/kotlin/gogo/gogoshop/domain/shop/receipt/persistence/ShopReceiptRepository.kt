0a16207852a1075303346271ce0e40d2946e473c
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-02-20 03:03:50 +0900

    add: findByShopIdAndStudentId method

--- src/main/kotlin/gogo/gogoshop/domain/shop/receipt/persistence/ShopReceiptRepository.kt
+++ src/main/kotlin/gogo/gogoshop/domain/shop/receipt/persistence/ShopReceiptRepository.kt
@@ -3,4 +3,5 @@ package gogo.gogoshop.domain.shop.receipt.persistence
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ShopReceiptRepository: JpaRepository<ShopReceipt, Long> {
+    fun findByShopIdAndStudentId(shopId: Long, studentId: Long): List<ShopReceipt>
 }
