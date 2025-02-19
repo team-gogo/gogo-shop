93a63abf781c1045f116e7c0cc1615bb7f058a67
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-02-20 02:02:23 +0900

    add: minigame mapper method

--- src/main/kotlin/gogo/gogoshop/domain/shop/root/application/ShopMapper.kt
+++ src/main/kotlin/gogo/gogoshop/domain/shop/root/application/ShopMapper.kt
@@ -2,17 +2,24 @@ package gogo.gogoshop.domain.shop.root.application
 
 
 
+import gogo.gogoshop.domain.cointoss.application.CoinTossMapper
 import gogo.gogoshop.domain.cointoss.application.dto.CoinTossResDto
 import gogo.gogoshop.domain.cointoss.persistence.CoinToss
+import gogo.gogoshop.domain.plinko.application.PlinkoMapper
 import gogo.gogoshop.domain.plinko.application.dto.PlinkoResDto
 import gogo.gogoshop.domain.plinko.persistence.Plinko
 import gogo.gogoshop.domain.shop.root.application.dto.ShopTicketStatusResDto
+import gogo.gogoshop.domain.yavarwee.application.YavarweeMapper
 import gogo.gogoshop.domain.yavarwee.application.dto.YavarweeResDto
 import gogo.gogoshop.domain.yavarwee.persistence.Yavarwee
 import org.springframework.stereotype.Component
 
 @Component
-class ShopMapper {
+class ShopMapper(
+    private val coinTossMapper: CoinTossMapper,
+    private val plinkoMapper: PlinkoMapper,
+    private val yavarweeMapper: YavarweeMapper,
+) {
 
     fun map(
         shopId: Long,
@@ -27,31 +34,13 @@ class ShopMapper {
             plinko = plinkoDto
         )
 
-    fun map(coinToss: CoinToss?) =
-        coinToss?.let {
-            CoinTossResDto(
-                coinTossId = it.coinTossId,
-                ticketPrice = it.ticketPrice,
-                ticketQuantity = it.ticketQauntity
-            )
-        }
+    fun mapCoinToss(coinToss: CoinToss?) =
+        coinTossMapper.map(coinToss)
 
-    fun map(plinko: Plinko?) =
-        plinko?.let {
-            PlinkoResDto(
-                plinkoId = it.plinkoId,
-                ticketPrice = it.ticketPrice,
-                ticketQuantity = it.ticketQauntity
-            )
-        }
+    fun mapPlinko(plinko: Plinko?) =
+        plinkoMapper.map(plinko)
 
-    fun map(yavarwee: Yavarwee?) =
-        yavarwee?.let {
-            YavarweeResDto(
-                yavarweeId = it.yavarweeId,
-                ticketPrice = it.ticketPrice,
-                ticketQuantity = it.ticketQauntity
-            )
-        }
+    fun mapYavarwee(yavarwee: Yavarwee?) =
+        yavarweeMapper.map(yavarwee)
 
 }
---
+++ src/main/kotlin/gogo/gogoshop/domain/yavarwee/application/YavarweeMapper.kt
@@ -0,0 +1,19 @@
+package gogo.gogoshop.domain.yavarwee.application
+
+import gogo.gogoshop.domain.yavarwee.application.dto.YavarweeResDto
+import gogo.gogoshop.domain.yavarwee.persistence.Yavarwee
+import org.springframework.stereotype.Component
+
+@Component
+class YavarweeMapper {
+
+    fun map(yavarwee: Yavarwee?) =
+        yavarwee?.let {
+            YavarweeResDto(
+                yavarweeId = it.yavarweeId,
+                ticketPrice = it.ticketPrice,
+                ticketQuantity = it.ticketQauntity
+            )
+        }
+
+}
