93cf49f6ecba914ff8a46408324fad86526edef7
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-03-01 22:06:40 +0900

    add: data class ShopTicketBuyEvent

---
+++ src/main/kotlin/gogo/gogoshop/domain/shop/root/event/ShopTicketBuyEvent.kt
@@ -0,0 +1,12 @@
+package gogo.gogoshop.domain.shop.root.event
+
+import gogo.gogoshop.domain.shop.receipt.persistence.TicketType
+
+data class ShopTicketBuyEvent(
+    val id: String,
+    val studentId: Long,
+    val shopId: Long,
+    val miniGameId: Long,
+    val ticketType: TicketType,
+    val purchaseQuantity: Int,
+)
