fd6dce94f3ddf474d0abdf848a5393682dd8d4dd
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-03-01 22:08:57 +0900

    add: shopTicketBuyEvent Event add

---
+++ src/main/kotlin/gogo/gogoshop/domain/shop/root/application/listener/ShopApplicationEventListener.kt
@@ -0,0 +1,24 @@
+package gogo.gogoshop.domain.shop.root.application.listener
+
+import gogo.gogoshop.domain.shop.root.event.ShopTicketBuyEvent
+import gogo.gogoshop.global.kafka.publisher.ShopPublisher
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
+
+@Component
+class ShopApplicationEventListener(
+    private val shopPublisher: ShopPublisher
+) {
+
+    private val log = LoggerFactory.getLogger(this::class.java.simpleName)
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    fun shopTicketBuyEvent(event: ShopTicketBuyEvent) {
+        with(event) {
+            log.info("published shop ticket buy application event: {}", id)
+            shopPublisher.publishShopTicketBuyEvent(event)
+        }
+    }
+}
