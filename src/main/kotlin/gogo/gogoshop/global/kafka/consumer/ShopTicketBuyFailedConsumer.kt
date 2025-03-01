29fd6aa690769aee13e6da649843df78ee35ebd9
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-03-01 23:06:35 +0900

    add: method KafkaListener setting

--- src/main/kotlin/gogo/gogoshop/global/kafka/consumer/ShopTicketBuyFailedConsumer.kt
+++ src/main/kotlin/gogo/gogoshop/global/kafka/consumer/ShopTicketBuyFailedConsumer.kt
@@ -6,6 +6,7 @@ import gogo.gogoshop.global.kafka.properties.KafkaTopics.SHOP_TICKET_BUY_FAILED
 import gogo.gogoshop.global.saga.ShopTicketBuyFailedSaga
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.listener.AcknowledgingMessageListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
@@ -19,6 +20,11 @@ class ShopTicketBuyFailedConsumer(
     private val log = LoggerFactory.getLogger(this::class.java.simpleName)
 
 
+    @KafkaListener(
+        topics = [SHOP_TICKET_BUY_FAILED],
+        groupId = "gogo",
+        containerFactory = "shopTicketBuyFailedEventListenerContainerFactory"
+    )
     override fun onMessage(data: ConsumerRecord<String, String>, acknowledgment: Acknowledgment?) {
         val (key, event) = data.key() to objectMapper.readValue(data.value(), ShopTicketBuyFailedEvent::class.java)
         log.info("${SHOP_TICKET_BUY_FAILED}_topic, key: $key, event: $event")
