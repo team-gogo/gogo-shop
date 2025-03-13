a71e9f95b9086f1d29410b0213fa365d33cb0e64
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-14 01:48:34 +0900

    update: point client url

--- src/main/kotlin/gogo/gogoshop/global/feign/client/PointClient.kt
+++ src/main/kotlin/gogo/gogoshop/global/feign/client/PointClient.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam
 
 @FeignClient(name = "gogo-stage")
 interface PointClient {
-    @GetMapping("/api/point/{stage_id}")
+    @GetMapping("/stage/api/point/{stage_id}")
     fun queryPointByStageIdAndStudentId(
         @PathVariable("stage_id") stageId: Long,
         @RequestParam("studentId") studentId: Long
