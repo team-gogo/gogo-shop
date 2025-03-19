06a49f1336f88acb8ea79f4543e6f91ea6d8c178
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-03-19 12:27:21 +0900

    fix: import bug

--- src/main/kotlin/gogo/gogoshop/domain/shop/root/application/ShopValidator.kt
+++ src/main/kotlin/gogo/gogoshop/domain/shop/root/application/ShopValidator.kt
@@ -3,13 +3,13 @@ package gogo.gogoshop.domain.shop.root.application
 import gogo.gogoshop.domain.shop.root.persistence.ActiveShopStatus
 import gogo.gogoshop.domain.shop.root.persistence.Shop
 import gogo.gogoshop.global.error.ShopException
-import gogo.gogoshop.global.internal.participant.api.ParticipantApi
+import gogo.gogoshop.global.internal.point.api.StageApi
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 
 @Component
 class ShopValidator(
-    private val participantApi: ParticipantApi
+    private val stageApi: StageApi
 ) {
 
     fun valid(point: Long, ticketPrice: Long, purchaseQauntity: Int, nowTicketQauntity: Int) {
@@ -31,7 +31,7 @@ class ShopValidator(
     }
 
     fun validStage(stageId: Long, studentId: Long) {
-        val isParticipant = participantApi.isParticipantByStageIdAndStudentId(stageId, studentId).isParticipant
+        val isParticipant = stageApi.isParticipantByStageIdAndStudentId(stageId, studentId).isParticipant
 
         if (isParticipant.not()) {
             throw ShopException("해당 유저는 스테이지에 참여 중이지 않습니다, stageId=$stageId, studentId=$studentId", HttpStatus.FORBIDDEN.value())
